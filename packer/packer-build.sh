1f478c3f527ed3f42036df79737f9e285c209cf1
Ruairidh MacLeod <ruairidh@rkm.dev>
2021-04-24 23:33:33 +0000

    add packer log

--- packer/packer-build.sh
+++ packer/packer-build.sh
@@ -4,7 +4,7 @@
 #Building image with packer
 cd /root/config
 packer fix template.json > template-fixed.json
-PACKER_LOG=1 packer build template-fixed.json
+PACKER_LOG=1 PACKER_LOG_PATH=/root/packer.log packer build template-fixed.json
 
 #Packer image build complete. Converting to VHD format.
 qemu-img convert -f raw -o subformat=fixed,force_size -O vpc disk/baked baked.vhd

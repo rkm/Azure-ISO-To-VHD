ff12e01ed17f5c006efe4c41ed224839c5762509
Ruairidh MacLeod <ruairidh@rkm.dev>
2021-04-30 21:12:12 +0000

    bump packer version

--- packer/install-packer.sh
+++ packer/install-packer.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-wget https://releases.hashicorp.com/packer/1.3.5/packer_1.3.5_linux_amd64.zip
-unzip packer_1.3.5_linux_amd64.zip
+set -euxo pipefail
+
+packer_version="1.7.2"
+
+wget https://releases.hashicorp.com/packer/"$packer_version"/packer_"$packer_version"_linux_amd64.zip
+unzip packer_"$packer_version"_linux_amd64.zip
 chmod +x packer
 mv packer /usr/local/bin
